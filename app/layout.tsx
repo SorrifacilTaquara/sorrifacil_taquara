36079db8e4f16608b68f7749e75fa41d25e264dc
carlos-augustow <carlos_augusto_wallauer@outlook.com>
2024-12-18 13:23:06 -0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,7 @@
-// import type { Metadata } from "next";
-// import "./globals.css";
-// import { montserrat } from "./fonts/fonts";
-
-// export const metadata: Metadata = {
-//   title: "Sorrifácil Taquara",
-//   description: "Sorrir é uma conquista",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="pt-br">
-//       <body className={`${montserrat.className} antialiased`}>
-//         <!-- Google Tag Manager (noscript) -->
-//             <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FRPJ89K"
-//             height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-//         <!-- End Google Tag Manager (noscript) -->
-//         {children}</body>
-//     </html>
-//   );
-// }
 import type { Metadata } from "next";
 import "./globals.css";
 import { montserrat } from "./fonts/fonts";
-import GoogleTagManager from "./GoogleTagManager.tsx"; // Ajuste o caminho conforme necessário
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Sorrifácil Taquara",
@@ -40,10 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`${montserrat.className} antialiased`}>
-        <GoogleTagManager />
-        {children}
-      </body>
+      <GoogleTagManager gtmId={"GTM-5FRPJ89K"} />
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }
