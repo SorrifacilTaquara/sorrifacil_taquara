59491d9410b992966c5bcff1a5a1ce5b153fc814
carlos-augustow <carlos_augusto_wallauer@outlook.com>
2025-08-25 10:42:12 -0300

    substituir Google Tag Manager por implementação direta com Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { montserrat } from "./fonts/fonts";
-import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Sorrifácil Taquara",
-  description: "Sorrir é uma conquista",
+  description: "",
 };
 
 export default function RootLayout({
@@ -15,8 +15,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <GoogleTagManager gtmId={"GTM-5FRPJ89K"} />
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <head />
+      <body className={`${montserrat.className} antialiased`}>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-53T6JBRX');
+          `}
+        </Script>
+
+        {/* NoScript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-53T6JBRX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }
